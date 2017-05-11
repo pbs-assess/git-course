0491bd41a6d4fef0ebc92590493a69405e9029f6
Chris Grandin <cgrandin@shaw.ca>
2017-05-11 07:36:17 -0700

    Remove bibtex and syntex from build scripts so that page numbering works correctly.

--- beamer/git-setup/build-pdf.bat
+++ beamer/git-setup/build-pdf.bat
@@ -1,1 +1,1 @@
-(@pdflatex -synctex=1 "git-course-setup.tex" && bibtex "git-course-setup" && pdflatex "git-course-setup.tex" && pdflatex "git-course-setup.tex") 1> latexOutput.log 2>&1
+(@pdflatex "git-course-setup.tex" && pdflatex "git-course-setup.tex" && pdflatex "git-course-setup.tex") 1> latexOutput.log 2>&1
