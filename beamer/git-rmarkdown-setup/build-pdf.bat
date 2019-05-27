5ee8bba83e52bbefb886ba3161ff864bd56478d9
Chris Grandin <cgrandin@shaw.ca>
2019-05-27 12:17:11 -0700

    Add rmarkdown beamer presentation

---
+++ beamer/git-rmarkdown-setup/build-pdf.bat
@@ -0,0 +1,1 @@
+(@pdflatex "git-rmarkdown-setup.tex" && pdflatex "git-rmarkdown-setup.tex" && pdflatex "git-rmarkdown-setup.tex") 1> latexOutput.log 2>&1
---
+++ beamer/git-rmarkdown-setup/clean.bat
@@ -0,0 +1,16 @@
+# Clean up the directory after a LaTeX build. Windows version
+del *.aux
+del *.bbl
+del *.blg
+del *.dvi
+del *.log
+del *.lot
+del *.lof
+del *.ps
+del *.toc
+del *.txt
+del *.out
+del *.nav
+del *.snm
+del *.synctex.gz*
+del git-rmarkdown-setup.pdf
